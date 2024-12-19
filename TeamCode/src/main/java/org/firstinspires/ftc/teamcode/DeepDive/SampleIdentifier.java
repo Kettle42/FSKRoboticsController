0b803f4c78b8beb4466d9f33c0f25c42ce603d3b
Kettle42 <fskrobotics595@gmail.com>
2024-12-19 15:54:17 -0500

    Various Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeepDive/SampleIdentifier.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeepDive/SampleIdentifier.java
@@ -1,12 +1,50 @@
 package org.firstinspires.ftc.teamcode.DeepDive;
 
 import android.annotation.TargetApi;
-import android.graphics.Color;
 
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 
 public class SampleIdentifier
 {
+    public static class Color
+    {
+        public final byte r;
+        public final byte g;
+        public final byte b;
+
+        public Color(int r, int g, int b)
+        {
+            this((byte)r, (byte)g, (byte)b);
+        }
+
+        public Color(byte r, byte g, byte b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        public Color subtract(Color other)
+        {
+            return new Color(this.r - other.r, this.g - other.g, this.b - other.b);
+        }
+
+        public Color add(Color other)
+        {
+            return new Color(this.r + other.r, this.g + other.g, this.b + other.b);
+        }
+
+        public double magnitude()
+        {
+            return Math.sqrt
+            (
+                Math.pow(this.r, 2.0) +
+                Math.pow(this.g, 2.0) +
+                Math.pow(this.b, 2.0)
+            );
+        }
+    }
+
     private final RevColorSensorV3 colorSensor;
 
     public SampleIdentifier(RevColorSensorV3 colorSensor)
@@ -14,9 +52,8 @@ public class SampleIdentifier
         this.colorSensor = colorSensor;
     }
 
-    @TargetApi(26)
     public Color getColor()
     {
-        return Color.valueOf(colorSensor.red(), colorSensor.green(), colorSensor.blue());
+        return new Color(colorSensor.red(), colorSensor.green(), colorSensor.blue());
     }
 }
